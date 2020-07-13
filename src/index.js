615b5790fbc7c257278f4f87258242ff0936b815
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-13 17:29:30 +0100

    Restructured files to access mock data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,63 @@
 const { ApolloServer } = require('apollo-server'); 
 const typeDefs = require('./schema'); 
-const resolvers = require('./resolvers');
+// const resolvers = require('./resolvers');
+// const data = require('./data');
+
+const notes = [
+  {
+    id: 0,
+    sender: 0,
+    receiver: 1,
+    status: 'DRAFT',
+    url: 'https://freesound.org/people/PaintingAir/sounds/527601/',
+  },
+  {
+    id: 1,
+    sender: 1,
+    receiver: 2,
+    status: 'RECORDED',
+    url: 'https://freesound.org/people/sholden/sounds/525062/',
+  },
+  {
+    id: 2,
+    sender: 2,
+    receiver: 0,
+    status: 'LISTENED',
+    url: 'https://freesound.org/people/Robinhood76/sounds/527203/',
+  },
+];
+
+const users =[
+  {
+    id: 0,
+    slackID: 'karla',
+    senderNotes: [0],
+    receiverNotes: [2],
+  },
+  {
+    id: 1,
+    slackID: 'alexa',
+    senderNotes: [1],
+    receiverNotes: [0],
+  },
+  {
+    id: 2,
+    slackID: 'ben',
+    senderNotes: [2],
+    receiverNotes: [1],
+  }
+];
+
+const resolvers = {
+  Query: {
+    notes: () => notes,
+    users: () => users,
+  }
+}
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers
 });
 
 server.listen().then(({ url }) => {
