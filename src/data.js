ac2eed02933b59b0de469271f211f5c4a455b477
Alexa <alexakearns@outlook.com>
2020-07-22 16:51:03 +0100

    Change data into new file and access on server

--- src/data.js
+++ src/data.js
@@ -1,44 +1,46 @@
-const notes = [
-  {
-    id: 0,
-    sender: 0,
-    receiver: 1,
-    status: 'DRAFT',
-    url: 'https://freesound.org/people/PaintingAir/sounds/527601/',
-  },
-  {
-    id: 1,
-    sender: 1,
-    receiver: 2,
-    status: 'RECORDED',
-    url: 'https://freesound.org/people/sholden/sounds/525062/',
-  },
-  {
-    id: 2,
-    sender: 2,
-    receiver: 0,
-    status: 'LISTENED',
-    url: 'https://freesound.org/people/Robinhood76/sounds/527203/',
-  },
-];
+  exports.notes= [
+    {
+      id: 0,
+      sender: 0,
+      receiver: 1,
+      status: 'DRAFT',
+      url: 'https://freesound.org/people/PaintingAir/sounds/527601/',
+    },
+    {
+      id: 1,
+      sender: 1,
+      receiver: 2,
+      status: 'RECORDED',
+      url: 'https://freesound.org/people/sholden/sounds/525062/',
+    },
+    {
+      id: 2,
+      sender: 2,
+      receiver: 0,
+      status: 'LISTENED',
+      url: 'https://freesound.org/people/Robinhood76/sounds/527203/',
+    },
+  ];
 
-const users =[
-  {
-    id: 0,
-    slackID: 'karla',
-    senderNotes: [0],
-    receiverNotes: [2],
-  }
-  {
-    id: 1,
-    slackID: 'alexa',
-    senderNotes: [1],
-    receiverNotes: [0],
-  },
-  {
-    id: 2,
-    slackID: 'ben',
-    senderNotes: [2],
-    receiverNotes: [1],
-  }
-];
+  exports.users =[
+    {
+      id: 0,
+      slackID: 'karla',
+      senderNotes: [0],
+      receiverNotes: [2],
+    },
+    {
+      id: 1,
+      slackID: 'alexa',
+      senderNotes: [1],
+      receiverNotes: [0],
+    },
+    {
+      id: 2,
+      slackID: 'ben',
+      senderNotes: [2],
+      receiverNotes: [1],
+    }
+  ];
+
+
--- src/index.js
+++ src/index.js
@@ -1,63 +1,20 @@
-const { ApolloServer } = require('apollo-server'); 
-const typeDefs = require('./schema'); 
+const { ApolloServer } = require("apollo-server");
+
+const typeDefs = require("./schema");
+
 // const resolvers = require('./resolvers');
-// const data = require('./data');
-
-const notes = [
-  {
-    id: 0,
-    sender: 0,
-    receiver: 1,
-    status: 'DRAFT',
-    url: 'https://freesound.org/people/PaintingAir/sounds/527601/',
-  },
-  {
-    id: 1,
-    sender: 1,
-    receiver: 2,
-    status: 'RECORDED',
-    url: 'https://freesound.org/people/sholden/sounds/525062/',
-  },
-  {
-    id: 2,
-    sender: 2,
-    receiver: 0,
-    status: 'LISTENED',
-    url: 'https://freesound.org/people/Robinhood76/sounds/527203/',
-  },
-];
-
-const users =[
-  {
-    id: 0,
-    slackID: 'karla',
-    senderNotes: [0],
-    receiverNotes: [2],
-  },
-  {
-    id: 1,
-    slackID: 'alexa',
-    senderNotes: [1],
-    receiverNotes: [0],
-  },
-  {
-    id: 2,
-    slackID: 'ben',
-    senderNotes: [2],
-    receiverNotes: [1],
-  }
-];
+const data = require("./data");
 
 const resolvers = {
   Query: {
-    notes: () => notes,
-    users: () => users,
-  }
-}
+    notes: () => data.notes,
+    users: () => data.users,
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 server.listen().then(({ url }) => {
