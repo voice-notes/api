c3207b47c042b4a0937eba15b5c260b646a71dc9
Alexa <alexakearns@outlook.com>
2020-07-23 17:46:35 +0100

    Converted to typescript. Apollo s module not found

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { ApolloServer } from "apollo-server";
+
+import typeDefs from "./schema";
+
+import resolvers from './resolvers';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🎙 Server ready at ${url}`);
+});
