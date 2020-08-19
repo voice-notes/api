f9da7030b49ccf63048db3c7b39d376fff76198f
joannacollier <collj035@gmail.com>
2020-08-19 15:30:56 +0100

    Initial set up of express

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
-import { ApolloServer } from "apollo-server";
-
+// import { ApolloServer } from "apollo-server";
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 import typeDefs from "./schema";
-
 import resolvers from './resolvers';
+
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-server.listen().then(({ url }) => {
-  console.log(`🎙 Server ready at ${url}`);
-});
+server.applyMiddleware({app});
+
+console.log(app)
+
+app.listen({port: 4000}, () => 
+  console.log(`🎙 Server ready at port 4000`)
+);
+
+
+
+
+// server.listen().then(({ url }) => {
+//   console.log(`🎙 Server ready at ${url}`);
+// });
