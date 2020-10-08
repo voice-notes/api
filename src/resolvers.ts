ff14b63144037405b1a981713ba57f487e571f24
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-10-08 16:07:09 +0100

    Fixed issue with the save method in resolvers.ts + added some notes about going forward

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -11,7 +11,7 @@ export default {
   Mutation: {
     createNote: (_: string, { sender, receiver, status, url }: INote) => {
       const note = new Note({ sender, receiver, status, url });
-      return note.save;
+      return note.save();
       // have code pushing this note ID to sender - sent array
       // reciever - recievedArray
     },
@@ -23,3 +23,6 @@ export default {
     },
   },
 };
+
+//We think for this to work, on createNote, the sender and receiver would need to be fished out of the
+//DB before we create a new note, as the sender + receiver are mapped to MongoID not SlackID
