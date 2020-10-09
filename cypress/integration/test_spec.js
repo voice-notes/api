85c4461acbd097db0064863cf428c55322a91b54
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-10-09 15:25:40 +0100

    Starting to get cypress working, test is hitting graphQL but we have no data in the DB

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -1,15 +1,19 @@
 describe("Server set up", () => {
-  beforeEach(() => {
-    cy.server();
-
-    cy.fixture("../../src/data.ts").as("data");
-    cy.route("POST", "graphql", "@data");
-
-    cy.visit("/graphql", {
-      onBeforeLoad: win => {
-        console.log("In the Visit");
-        win.fetch = null;
-      },
+  it("connects to the graphql server and prints", () => {
+    cy.log("Hello World");
+    const query = `
+      {
+        notes{
+          id
+        }
+      }
+    `;
+    cy.request({
+      url: "http://localhost:4000/graphql/", // graphql endpoint
+      body: { query }, // or { query: query } depending if you are writing with es6
+      failOnStatusCode: false, // not a must but in case the fail code is not 200 / 400
+    }).then((res) => {
+      cy.log(res);
     });
   });
 });
