410a43449796dd47a39cf096055c96bd4b258df0
Tim <timcpb@hotmail.com>
2020-10-12 23:12:10 +0100

    Specify post method in cy.request

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -27,11 +27,13 @@ describe("Server set up", () => {
       
     }`
     cy.request({
+      method: "post",
       url: "http://localhost:4000/graphql/", // graphql endpoint
-      body: { mutation }, // or { query: query } depending if you are writing with es6
+      body: { query }, // or { query: query } depending if you are writing with es6
       failOnStatusCode: false, // not a must but in case the fail code is not 200 / 400
     }).then((res) => {
-      cy.log(res.user);
+      console.log(res)
+      cy.log(res);
     });
   });
 });
