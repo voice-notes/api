d03bafdce8a365d050be07c00a3734826f782258
joannacollier <collj035@gmail.com>
2020-10-16 16:37:54 +0100

    Taken promise away, data query in create new note returning correct data

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -11,42 +11,18 @@ export default {
 
   Mutation: {
     createNote: (_:string, {sender, receiver, status, url}:INote) => {
+      const slackTempID: Array<string> = [sender]
 
-      let p = new Promise((resolve, reject) =>{
+      User.find().where('slackID').in(slackTempID).exec((err, records) => {
 
-        const slackTempID: Array<string> = [sender]
+        let senderMongoID = records[0]._id
 
-        User.find().where('slackID').in(slackTempID).exec((err, records) => {
-          
-          if(err){
-            reject(err)
-          } else {
-            if(records.length){
-            
-              let senderMongoID = records[0]._id
-  
-              console.log(`unicorn = ${records}`)
-              console.log(`senderID = ${senderMongoID}`)
+        console.log(`unicorn = ${records}`)
+        console.log(`senderID = ${senderMongoID}`)
 
-              const note = new Note({sender, receiver, status, url});
-              return note.save()
-            } else {
-              resolve(null)
-            }
-          }
-
-
-
-          // let receiverID = "5f3e92d4fc80444d56f7c562"
-          // let senderMongoID = records._id
-  
-          // console.log(`unicorn = ${records}`)
-          // console.log(`senderID = ${senderMongoID}`)
-          
-
-  
-        });
-      }) 
+        const note = new Note({sender, receiver, status, url});
+        return note.save()
+      });
     },
     createUser: (_:string, {slackID}:IUser) => {
       const senderNotes: Array<string> = [] 
