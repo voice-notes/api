402eced6f2c188b0b5aa738759cf68fa031473a0
joannacollier <collj035@gmail.com>
2020-10-16 14:48:57 +0100

    Changed mongo DB note model to store sender/ reciever as string instead of mongoDB ID

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -11,16 +11,19 @@ export default {
   Mutation: {
     createNote: (_:string, {sender, receiver, status, url}:INote) => {
       const note = new Note({sender, receiver, status, url});
-      return note.save
+      return note.save()
       // have code pushing this note ID to sender - sent array 
       // reciever - recievedArray 
 
     },
     createUser: (_:string, {slackID}:IUser) => {
-      const senderNotes: Array<string> = []
+      const senderNotes: Array<string> = [] 
       const receiverNotes: Array<string> = []
       const user = new User({slackID, senderNotes, receiverNotes});
       return user.save()
     }
   }
 }
+
+// senderNotes: [ID]
+// receiverNotes: [ID]
