ca4a5d55b5ebc8895f534d365c992cc625bdbb74
Angelica <Angelica137@users.noreply.github.com>
2020-11-06 16:30:12 +0000

    refactor test expectation

--- cypress/integration/query_spec.js
+++ cypress/integration/query_spec.js
@@ -5,19 +5,35 @@ describe("Can retrieve users", () => {
     const query = `
       {
         users{
-          slackID
+					slackID
+					senderNotes
+					receiverNotes
         }
       }
     `;
 
+    const expectation = {
+      users: [
+        {
+          slackID: "TestSender",
+          senderNotes: [],
+          receiverNotes: [],
+        },
+        {
+          slackID: "TestReceiver",
+          senderNotes: [],
+          receiverNotes: [],
+        },
+      ],
+    };
     cy.request({
       method: "post",
       url: GRAPHQL_ENDPOINT, // graphql endpoint
       body: { query }, // or { query: query } depending if you are writing with es6
       failOnStatusCode: false, // not a must but in case the fail code is not 200 / 400
     }).then((res) => {
-      expect(res.body.data.users[0].slackID).to.equal("TestUser");
-      expect(res.body.data.users[1].slackID).to.equal("TestUser2");
+      cy.log(res.body.data);
+      expect(res.body.data).to.deep.equal(expectation);
     });
   });
 });
