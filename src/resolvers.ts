8f1bebab1e70c04a22001949f5d5fe9bbeba2b5d
Tim <timcpb@hotmail.com>
2020-11-26 14:42:20 +0000

    linting

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,19 +1,24 @@
-import { Note, INote } from './models/note'
-import { User, IUser } from './models/user'
-import { response } from 'express'
+import { Note, INote } from "./models/note";
+import { User, IUser } from "./models/user";
+import { response } from "express";
 
-function createNote(sender: IUser, receiver: IUser, status: string, url: string){
-  if(sender._id != null && receiver._id != null){
+function createNote(
+  sender: IUser,
+  receiver: IUser,
+  status: string,
+  url: string
+) {
+  if (sender._id != null && receiver._id != null) {
     return new Note({
       sender: sender._id,
       receiver: receiver._id,
       senderSlackID: sender.slackID,
       receiverSlackID: receiver.slackID,
       status,
-      url
-    }).save()
+      url,
+    }).save();
   }
-} 
+}
 
 export default {
   Query: {
@@ -23,27 +28,26 @@ export default {
   },
 
   Mutation: {
-    createNote: async(_:string, args:INote) => {
-      const {sender, receiver, status, url} = args
-  
-      const dbSender = await User.findOne({slackID: sender})
-      const dbReceiver = await User.findOne({slackID: receiver})
+    createNote: async (_: string, args: INote) => {
+      const { sender, receiver, status, url } = args;
+
+      const dbSender = await User.findOne({ slackID: sender });
+      const dbReceiver = await User.findOne({ slackID: receiver });
 
       if (dbSender != null && dbReceiver != null) {
-        const note = await createNote(dbSender, dbReceiver, status, url)
+        const note = await createNote(dbSender, dbReceiver, status, url);
         if (note != undefined) {
-          dbSender.senderNotes.push(note._id)
-          dbReceiver.receiverNotes.push(note._id)
-          await dbSender.save()
-          await dbReceiver.save()
+          dbSender.senderNotes.push(note._id);
+          dbReceiver.receiverNotes.push(note._id);
+          await dbSender.save();
+          await dbReceiver.save();
         }
-        return note
+        return note;
       }
-
     },
-    createUser: (_:string, {slackID}:IUser) => {
-      const user = new User({slackID});
-      return user.save()
-    }
-  }
-}
+    createUser: (_: string, { slackID }: IUser) => {
+      const user = new User({ slackID });
+      return user.save();
+    },
+  },
+};
