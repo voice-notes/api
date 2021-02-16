9dd42aa05656a144a6a8d57da39f1f2ea7eb9534
Tim Bishop <timcpb@hotmail.com>
2021-02-16 09:28:53 +0000

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import { IUser } from "./models/user";
 import { Note } from "./models/note";
+import { Request, Response } from "express";
 
 export const createMongoNoteInstance = async (
   sender: IUser,
@@ -14,5 +15,31 @@ export const createMongoNoteInstance = async (
       status,
       url,
     }).save();
+  }
+};
+
+const returnUrlParameters = (url: string) => {
+  const splitUrl = url.split("/");
+  const index = splitUrl.length;
+  return [splitUrl[index - 3], splitUrl[index - 2], splitUrl[index - 1]];
+};
+
+export const slackQuery = (request: Request, response: Response) => {
+  const { body } = request;
+  if (body.response_url) {
+    const [param1, param2, param3] = returnUrlParameters(body.response_url);
+
+    console.log(request.body);
+    response.send({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Hey <@${body.user_id}>\n<http://localhost:3000/?chan=${body.channel_id}&sender=${body.user_id}&p1=${param1}&p2=${param2}&p3=${param3}|Tape your message here!>\n:loud_sound:`,
+          },
+        },
+      ],
+    });
   }
 };
