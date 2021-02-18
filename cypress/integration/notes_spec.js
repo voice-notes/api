224ef1b8c04533bed2294783cce6ae85cc884f20
Tim <timcpb@hotmail.com>
2021-02-18 14:28:40 +0000

    Remove test relateing to deleted User model and alter expectation to match updated Note model

--- cypress/integration/notes_spec.js
+++ cypress/integration/notes_spec.js
@@ -5,8 +5,8 @@ import { ADD_NOTE } from "../support/mutations";
 describe("Can add note", () => {
   it("Returns expected data on createNote mutation", () => {
     const expectation = {
-      status: "RECORDED",
-      url: "TEST",
+      slackID: "TestSender",
+      audioUrl: "Hello, World!",
     };
     cy.request({
       method: "post",
@@ -14,18 +14,6 @@ describe("Can add note", () => {
       body: { query: ADD_NOTE },
     }).then((res) => {
       expect(res.body.data.createNote).to.deep.equal(expectation);
-    });
-  });
-
-  it("Adds Note ID to the User array", () => {
-    cy.request({
-      method: "post",
-      url: GRAPHQL_ENDPOINT,
-      body: { query: USERS },
-    }).then((res) => {
-      const { users } = res.body.data;
-      expect(users[0].senderNotes.length).to.equal(1);
-      expect(users[1].receiverNotes.length).to.equal(1);
     });
   });
 });
