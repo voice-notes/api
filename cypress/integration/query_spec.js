2dc5b65dbf84616a770fd024033d700c95e57b9a
Tim <timcpb@hotmail.com>
2021-02-18 14:31:18 +0000

    Adjust expcetation to match updated Note model

--- cypress/integration/query_spec.js
+++ cypress/integration/query_spec.js
@@ -1,26 +1,26 @@
 import { GRAPHQL_ENDPOINT } from "../../src/constants";
-import { USERS } from "../support/queries";
+import { NOTES } from "../support/queries";
 
 describe("Can retrieve users", () => {
   it("returns users when user query is fired", () => {
     const expectation = {
-      users: [
+      notes: [
         {
-          slackID: "TestSender",
-          senderNotes: [],
-          receiverNotes: [],
+          slackID: "TestSender1",
+          audioUrl: "TestURL1",
+          responseUrl: "ResponseURL1",
         },
         {
-          slackID: "TestReceiver",
-          senderNotes: [],
-          receiverNotes: [],
+          slackID: "TestSender2",
+          audioUrl: "TestURL2",
+          responseUrl: "ResponseURL2",
         },
       ],
     };
     cy.request({
       method: "post",
       url: GRAPHQL_ENDPOINT,
-      body: { query: USERS },
+      body: { query: NOTES },
     }).then((res) => {
       expect(res.body.data).to.deep.equal(expectation);
     });
