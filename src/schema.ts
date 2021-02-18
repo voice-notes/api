7df3d96d2c7a66cd3be3cfd1f2de5e50a7fddea5
charlie-galb <galbraith.charlie@gmail.com>
2021-02-18 12:27:58 +0000

    Remove user, sender, receiver

--- src/schema.ts
+++ src/schema.ts
@@ -3,33 +3,20 @@ import { gql } from "apollo-server-express";
 const typeDefs = gql`
   type Query {
     notes: [Note]
-    users: [User]
-    test: String
-  }
-
-  type Note {
-    id: ID!
-    sender: String!
-    receiver: String!
-    status: String!
-    url: String!
-  }
-
-  type User {
-    id: ID!
-    slackID: String
-    senderNotes: [ID]
-    receiverNotes: [ID]
   }
 
   type Mutation {
     createNote(
-      sender: String!
-      receiver: String!
-      status: String!
-      url: String!
+      slackID: String!
+      audioUrl: String!
+      responseUrl: String!
     ): Note!
-    createUser(slackID: String!): User!
+  }
+
+  type Note {
+    id: ID!
+    slackID: String
+    url: String!
   }
 `;
 
