efec9115af7873f8d6527b9eee01f2974ff6fb51
Tim <timcpb@hotmail.com>
2021-02-24 14:43:47 +0000

    Replace MONGO_URL with MONGO_PRODUCTION_URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
-export const MONGO_URL = "mongodb://localhost/test";
+export const MONGO_PRODUCTION_URL =
+  "mongodb+srv://TapedItTeam:SharpenedPencil99@cluster0.8rwcu.mongodb.net/TapedItProductionDB?retryWrites=true&w=majority";
+export const MONGO_TEST_URL =
+  "mongodb+srv://TapedItTeam:SharpenedPencil99@cluster0.8rwcu.mongodb.net/TapedItTestDB?retryWrites=true&w=majority";
 
 export const GRAPHQL_ENDPOINT = "http://localhost:4000/graphql/";
