7bd8326bbe1df410b686370bb7881a91c7d69d70
charlie-galb <galbraith.charlie@gmail.com>
2021-02-25 13:39:31 +0000

    Add function to toggle database depending on NODE_ENV

---
+++ src/utils/returnDatabaseUri.ts
@@ -0,0 +1,7 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const returnDatabaseUri = () => {
+    return process.env.NODE_ENV === "production" ?  process.env.MONGO_PRODUCTION_URI : process.env.MONGO_TEST_URI
+}
