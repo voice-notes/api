cd071a6015c8968b7c1fc53f80344896a6493de8
charlie-galb <galbraith.charlie@gmail.com>
2021-03-03 16:54:19 +0000

    Replace hardcoded url with env variable

--- src/utils/slackQuery.ts
+++ src/utils/slackQuery.ts
@@ -3,16 +3,16 @@ import { Request, Response } from "express";
 
 export const slackQuery = (request: Request, response: Response) => {
   const { user_id, response_url } = request.body;
+  const { FRONT_END_URL } = process.env
   if (response_url) {
     const [param1, param2, param3] = returnUrlParameters(response_url);
-
     response.send({
       blocks: [
         {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: `<http://localhost:3000/?&sender=${user_id}&p1=${param1}&p2=${param2}&p3=${param3}|Tape your message here!>`,
+            text: `<http://${FRONT_END_URL}/?&sender=${user_id}&p1=${param1}&p2=${param2}&p3=${param3}|Tape your message here!>`,
           },
         },
       ],
