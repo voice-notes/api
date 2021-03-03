716bb34fed3688f4e8699ee01ebb3dbeb5924879
charlie-galb <galbraith.charlie@gmail.com>
2021-03-03 16:38:02 +0000

    Remove console.logs

--- src/utils/returnDatabaseUri.ts
+++ src/utils/returnDatabaseUri.ts
@@ -3,8 +3,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const returnDatabaseUri = () => {
-  console.log(process.env.MONGO_TEST_URI);
-  console.log(process.env.GRAPHQL_ENDPOINT);
   return process.env.NODE_ENV === "production"
     ? process.env.MONGO_PRODUCTION_URI
     : process.env.MONGO_TEST_URI;
