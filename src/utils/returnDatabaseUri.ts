103b38ca716a92fb4100f8ba8c6c55385000792c
Tim <timcpb@hotmail.com>
2021-03-03 19:57:16 +0000

    remove unnecesary import of dotenv

--- src/utils/returnDatabaseUri.ts
+++ src/utils/returnDatabaseUri.ts
@@ -1,7 +1,3 @@
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
 export const returnDatabaseUri = () => {
   return process.env.NODE_ENV === "production"
     ? process.env.MONGO_PRODUCTION_URI
