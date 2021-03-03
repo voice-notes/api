ac38043ffc937ec48a1c5ce2c14e6155d111931e
Tim <timcpb@hotmail.com>
2021-03-03 15:39:37 +0000

    Add console.logs to test env vars

--- src/utils/returnDatabaseUri.ts
+++ src/utils/returnDatabaseUri.ts
@@ -3,5 +3,9 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const returnDatabaseUri = () => {
-    return process.env.NODE_ENV === "production" ?  process.env.MONGO_PRODUCTION_URI : process.env.MONGO_TEST_URI
-}
+  console.log(process.env.MONGO_TEST_URI);
+  console.log(process.env.GRAPHQL_ENDPOINT);
+  return process.env.NODE_ENV === "production"
+    ? process.env.MONGO_PRODUCTION_URI
+    : process.env.MONGO_TEST_URI;
+};
