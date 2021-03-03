83460ec875240e5d5e158ff8e5699d155819a464
charlie-galb <galbraith.charlie@gmail.com>
2021-03-03 16:37:52 +0000

    Delete unnecessary function

--- src/utils/returnGraphqlEndpoint.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-export const returnGraphqlEndpoint = () => {
-  console.log(process.env.MONGO_TEST_URI);
-  console.log(process.env.GRAPHQL_ENDPOINT);
-  return process.env.GRAPHQL_ENDPOINT;
-};
