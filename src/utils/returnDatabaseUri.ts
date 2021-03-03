96eccd3b1dd8c57eb185dc49f440ea0f0e642dfc
Tim <timcpb@hotmail.com>
2021-03-03 17:36:07 +0000

    import env vars

--- src/utils/returnDatabaseUri.ts
+++ src/utils/returnDatabaseUri.ts
@@ -1,3 +1,7 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 export const returnDatabaseUri = () => {
   return process.env.NODE_ENV === "production"
     ? process.env.MONGO_PRODUCTION_URI
